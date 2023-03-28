f9d8ab048f73320b1b6b0bf001cf0560ed469131
“Anna <anna.wlodarczyk.dev@gmail.com>
2023-03-28 14:05:22 +0100

    Add multiple_pointers branch and count unique values

--- index.js
+++ index.js
@@ -1,41 +1,42 @@
 console.log('xx')
 
-function validAnagram(str1, str2) {
-    if (str1.length != str2.length) {
-        return false;
-    }
-    let frequencyCounter1 = {};
-    let frequencyCounter2 = {};
+// function countUniqueValues(arr) {
 
-    for (let char of str1) {
-        frequencyCounter1[char] = (frequencyCounter1[char] || 0) + 1
-    }
-    for (let char of str2) {
-        frequencyCounter2[char] = (frequencyCounter2[char] || 0) + 1
-    }
+//     let unique = []
+//     for (let i = 0; i < arr.length; i++) {
+//         let first = arr[i];
+//         let next = arr[i + 1];
 
-    // console.log(frequencyCounter1)
-    // console.log(frequencyCounter2)
+//         if (first != next) {
+//             unique.push(first)
+//         }
 
-    for (let key in frequencyCounter1) {
-        if (!(key in frequencyCounter2)) {
-            return false
+//     }
+//     console.log(unique)
+// }
+
+// console.log(countUniqueValues([-1, 0, 0, 1, 2, 3, 4, 5, 6, 6]))
+// console.log(countUniqueValues([1, 1, 1, 1, 1, 2]))
+// console.log(countUniqueValues([1, 1, 1, 1, 1, 2])) //2
+
+// console.log(countUniqueValues([])) // 0
+// console.log(countUniqueValues([-2, -1, -1, 0, 1])) // 4
+
+
+function countUniqueValues(arr) {
+    if (arr.length == 0) return 0
+    let i = 0
+    for (j = 1; j < arr.length; j++) {
+        if (arr[i] != arr[j]) {
+            i++;
+            arr[i] = arr[j]
         }
-        if (frequencyCounter1[key] != frequencyCounter2[key]) {
-            return false
-
-        }
     }
-    return true;
+    return i + 1
 }
 
-console.log(validAnagram('anagram', 'aaagram')); // false
-console.log(validAnagram('anagram', 'anagram')); // true
-console.log(validAnagram('', '')) // true
-console.log(validAnagram('aaz', 'zza')) // false
-console.log(validAnagram('anagram', 'nagaram')) // true
-console.log(validAnagram("rat", "car")) // false 
-console.log(validAnagram('awesome', 'awesom')) // false
-console.log(validAnagram('amanaplanacanalpanama', 'acanalmanplanpamana')) // false
-console.log(validAnagram('qwerty', 'qeywrt')) // true
-console.log(validAnagram('texttwisttime', 'timetwisttext')) // true
+console.log(countUniqueValues([-1, 0, 0, 1, 2, 3, 4, 5, 6, 6])) //8
+console.log(countUniqueValues([1, 1, 1, 1, 1, 2])) //2
+
+console.log(countUniqueValues([])) // 0
+console.log(countUniqueValues([-2, -1, -1, 0, 1])) // 4
