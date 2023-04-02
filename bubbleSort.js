a4a0cc646e70aaadc7db49dfb8a44ee10a0be3ff
“Anna <anna.wlodarczyk.dev@gmail.com>
2023-04-02 17:19:24 +0100

    add noSwaps variable to bubbleSort function

--- bubbleSort.js
+++ bubbleSort.js
@@ -7,6 +7,7 @@
 
 
 function bubbleSort(arr) {
+    let noSwaps
     for (var i = arr.length; i > 0; i--) { // i = 4 
         for (var j = 0; j < i - 1; j++) { // j = 3 comparisons
             console.log(arr, arr[j], arr[j + 1]);
@@ -14,27 +15,30 @@ function bubbleSort(arr) {
                 var temp = arr[j];
                 arr[j] = arr[j + 1];
                 arr[j + 1] = temp;
+                noSwaps = false
             }
         }
+        if (noSwaps) break
     }
     return arr;
 }
 
 // ES2015 Version
-function bubbleSort(arr) {
-    const swap = (arr, idx1, idx2) => {
-        [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
-    };
+// function bubbleSort(arr) {
+//     const swap = (arr, idx1, idx2) => {
+//         [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]];
+//     };
 
-    for (let i = arr.length; i > 0; i--) {
-        for (let j = 0; j < i - 1; j++) {
-            if (arr[j] > arr[j + 1]) {
-                swap(arr, j, j + 1);
-            }
-        }
-    }
-    return arr;
-}
+//     for (let i = arr.length; i > 0; i--) {
+//         for (let j = 0; j < i - 1; j++) {
+//             if (arr[j] > arr[j + 1]) {
+//                 swap(arr, j, j + 1);
+//             }
+//         }
+//     }
+//     return arr;
+// }
 
 
 console.log(bubbleSort([37, 45, 29, 8]))
+console.log(bubbleSort([8, 1, 2, 3, 4, 5, 6, 7]))
