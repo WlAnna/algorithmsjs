8cde88ab2cfacb0fb25c83170a45dd7243fe2cea
“Anna <anna.wlodarczyk.dev@gmail.com>
2023-04-04 11:06:27 +0100

    add methosds push, pop, shift, unshift

--- linkedList.js
+++ linkedList.js
@@ -8,6 +8,7 @@ class Node {
     }
 }
 
+// push()
 // This function should accept a value
 // Create a new node using the value passed to the function
 // If there is no head property on the list, set the head and tail to be the newly created node
@@ -15,10 +16,34 @@ class Node {
 // Increment the length by one
 // Return linked list
 
+// pop()
+// If there are no nodes in the list, return undefined
+// Loop trough the list until you reach the tail
+// Set the next property of the 2nd to last nore to be null
+// Set the tail to be the 2nd to last node
+// Decrement the length of the list by 1
+// Return the value of the node removed
+
+// shift()
+// If there are no nodes, return undefined
+// Store the current head property in a variable
+// Set the head property to be the current head's next property
+// Decrement the length by 1
+// Return the value of the node removed
+
+// unshift()
+// This function should accept a value
+// Create a new node using the value passed to the function
+// If there is no head property on the list, set the head and tail to be the newly created node
+// Otherwise set the newly created node's next property to be the current head property on the list
+// Set the head property on the list to be the newly created node
+// Increment the length of the list by 1
+// Return the linked list
+
 class SinglyLinkedList {
     constructor() {
-        this.head = null;
-        this.tail = null;
+        this.head = null; // poczatek
+        this.tail = null; // current lub end
         this.length = 0;
     }
 
@@ -34,6 +59,55 @@ class SinglyLinkedList {
         this.length++;
         return this;
     }
+
+    pop() {
+        if (!this.length) return undefined
+
+        let current = this.head
+        let newTail = current
+        while (current.next) {
+            newTail = current
+            current = current.next
+        }
+
+        console.log(current.val)
+        console.log(newTail.val)
+
+        this.tail = newTail
+        this.tail.next = null
+
+        this.length--
+            if (this.length == 0) {
+                this.head = null;
+                this.tail = null;
+            }
+        return current
+    }
+
+    shift() {
+        if (!this.head) return undefined;
+        let currentHead = this.head;
+        this.head = currentHead.next;
+        this.length--;
+        if (this.length === 0) {
+            this.tail = null;
+        }
+        return currentHead;
+    }
+
+    unshift(val) {
+        let newNode = new Node(val)
+        if (!this.head) {
+            this.head = newNode;
+            this.tail = this.head;
+        } else {
+            newNode.next = this.head; // newNode points to the current Head
+            this.head = newNode; // we update the head
+        }
+        this.length++;
+        return this;
+
+    }
 }
 
 // var first = new Node("Hi")
@@ -48,3 +122,8 @@ console.log(list.push("GOODBYE"))
 console.log(list.push(99))
 console.log(list.head.next)
 console.log(list.head.next.next)
+
+console.log(list.pop())
+console.log(list.shift())
+
+console.log(list.unshift('xx'))
