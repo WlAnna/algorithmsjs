2f4da98f35836f62b167929d4bfb199d504324f3
“Anna <anna.wlodarczyk.dev@gmail.com>
2023-04-06 16:46:07 +0100

    create binarySearchTree and add method insert

---
+++ binarySearchTree.js
@@ -0,0 +1,87 @@
+class Node {
+    constructor(value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null;
+    }
+
+    insert(val) {
+        let newNode = new Node(val)
+        if (!this.root) {
+            this.root = newNode
+            return this
+        } else {
+            let current = this.root
+            while (true) {
+                if (val === current.value) return undefined
+                if (val < current.value) {
+                    if (current.left === null) {
+                        current.left = newNode
+                        return this
+                    } else {
+                        current = current.left
+                    }
+                } else if (val > current.value) {
+                    if (current.right === null) {
+                        current.right = newNode
+                        return this
+                    } else {
+                        current = current.right
+                    }
+                }
+            }
+        }
+    }
+
+    // insert(value) {
+    //     var newNode = new Node(value);
+    //     if (this.root === null) {
+    //         this.root = newNode;
+    //         return this;
+    //     }
+    //     var current = this.root;
+    //     while (true) {
+    //         if (value === current.value) return undefined;
+    //         if (value < current.value) {
+    //             if (current.left === null) {
+    //                 current.left = newNode;
+    //                 return this;
+    //             }
+    //             current = current.left;
+    //         } else {
+    //             if (current.right === null) {
+    //                 current.right = newNode;
+    //                 return this;
+    //             }
+    //             current = current.right;
+    //         }
+    //     }
+    // }
+
+}
+
+
+var tree = new BinarySearchTree();
+// tree.root = new Node(10);
+// tree.root.right = new Node(15);
+// tree.root.left = new Node(7);
+// tree.root.left.right = new Node(9);
+
+//     10
+//  5      13
+// 2  7  11  16
+//  3
+tree.insert(10)
+tree.insert(5)
+tree.insert(13)
+tree.insert(11)
+tree.insert(16)
+tree.insert(2)
+tree.insert(7)
+console.log(tree.insert(10))
