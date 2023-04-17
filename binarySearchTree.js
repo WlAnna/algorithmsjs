34dd9f59d03115f04b00625fc614a42bff0740c9
“Anna <anna.wlodarczyk.dev@gmail.com>
2023-04-17 14:32:33 +0100

    add binary search tree and tree traversal methods

--- binarySearchTree.js
+++ binarySearchTree.js
@@ -1,3 +1,77 @@
+// Inserting a node - iteratively or recursively
+// Create a new node
+// Starting at the root
+// - Check if there is a root, if not - the root now becomes that new node
+// - If there is a root, check if the value of the new node is grater than or less than the value of the root
+// - If it is grater
+//   Check to see if there is a node to the right
+//    If there is, move to that node an repeat these steps
+//    If there is not, add that node as the right property
+// - If it is less
+//   Check to see of there is a node to the left
+//    If there is, move to that node and repeat these steps
+//    If there is not, add as the left property
+
+// Finding a node in a BST (Breadth First Search)
+// Steps - iteratively or Recursively
+// Starting at the root
+// - Checking if there is a root, if not - we're done searching!
+// - If there is a root, check if the value of the new node is the value we are searching for. If we found it, we are done!
+// - If not, check to see if the value is grater than or less than the value of the root
+// - If it is grater
+//   Check to see if there is a node to the right
+//     If there is, move to that node and repeat these steps
+//     If there is not, we're done searching!
+// - If it is less
+//   Check to see if there is a node to the left
+//     If there is, move to that node and repeat these steps
+//     If there is not, we're done searching!
+
+// BSF (Breadth First Search)
+// Steps - iteratively
+// Starting at the root
+// - Create a queue (this can be an array) and a variable to store the values of nodes visited
+// - Place the root node in the queue
+// - Loop as long as there is anything in the queue
+//  Dequeue a node from the queue and push the value of the node into the variable that stores the nodes
+//  If there is a left property on the node dequeued - add it to the queue
+//  If there is a right property on the node dequeued - add it to the queue
+
+// DFS - PreOrder
+// Steps - recursively
+// Create a variable to store the values of nodes visited
+// Store the root of the BST in a variable called current
+// Write a helper function which accepts a node
+// - Push the value of the node to the variable that stores the values
+// - If the node has a left property, call the helper function with the left property on the node
+// - If the node has a right property, call the helper function with the right property on the node
+// Invoke the helper function with the current variable
+// Return the array of value
+
+// DFS - PostOrder
+// Steps - Recursively
+// Create a variable to store the values of nodes visited
+// Store the root of the BST in a variable called current
+// Write a helper function which accepts a node
+// - If the node has a left property, call the helper function with the left property on the node
+// - If the node has a right property, call the helper function with the right property on the node
+// - Push the value of the node to the variable that stores the values
+// - Invoke the helper function with the current variable
+// Return the array of values
+
+// DFS - InOrder
+// Steps - Recursively
+// Create a variable to store the values of nodes visited
+// Store the root of the BST in a variable called current
+// Write a helper function which accepts a node
+// - If the node has a left property, call the helper function with the left property on the node
+// - Push the value of the node to the variable that stores the values
+// - If the node has a right property, call the helper function with the right property on the node
+// - Invoke the helper function with the current variable
+// Return the array of values
+
+
+
 class Node {
     constructor(value) {
         this.value = value;
@@ -96,6 +170,86 @@ class BinarySearchTree {
         }
         return false;
     }
+
+
+    BFS() {
+        let queue = []
+        let data = []
+        queue.push(this.root)
+        let node;
+        while (queue.length) {
+            node = queue.shift()
+            data.push(node.value)
+            if (node.left) {
+                queue.push(node.left)
+            }
+            if (node.right) {
+                queue.push(node.right)
+            }
+
+        }
+        return data
+
+    }
+
+    //DFS
+    preOder() {
+        let data = []
+        let current = this.root
+
+        function helper(node) {
+            data.push(node.value)
+            if (node.left) {
+                helper(node.left)
+            }
+            if (node.right) {
+                helper(node.right)
+            }
+        }
+
+        helper(current)
+        return data
+    }
+
+    //DFS
+    postOrder() {
+        let data = []
+        let current = this.root
+
+        function helper(node) {
+            if (node.left) {
+                helper(node.left)
+            }
+            if (node.right) {
+                helper(node.right)
+            }
+            data.push(node.value)
+        }
+
+        helper(current)
+        return data
+    }
+
+    //DFS
+    inOrder() {
+        let data = []
+        let current = this.root
+
+        function helper(node) {
+            if (node.left) {
+                helper(node.left)
+            }
+            data.push(node.value)
+            if (node.right) {
+                helper(node.right)
+            }
+
+        }
+
+        helper(current)
+        return data
+    }
+
 }
 
 
@@ -111,14 +265,29 @@ var tree = new BinarySearchTree();
 //  5      13
 // 2  7  11  16
 //  3
+// tree.insert(10)
+// tree.insert(5)
+// tree.insert(13)
+// tree.insert(11)
+// tree.insert(16)
+// tree.insert(2)
+// tree.insert(7)
+// console.log(tree.insert(10))
+// console.log(tree.find(2))
+// console.log(tree.find(7))
+// console.log(tree.find(15))
+
 tree.insert(10)
-tree.insert(5)
-tree.insert(13)
-tree.insert(11)
-tree.insert(16)
-tree.insert(2)
-tree.insert(7)
-console.log(tree.insert(10))
-console.log(tree.find(2))
-console.log(tree.find(7))
-console.log(tree.find(15))
+tree.insert(6)
+tree.insert(15)
+tree.insert(3)
+tree.insert(8)
+tree.insert(20)
+console.log(tree.BFS()) //[10, 6, 15, 3, 8, 20]
+console.log(tree.preOder()) //  [10, 6, 3, 8, 15, 20]
+console.log(tree.postOrder()) //  [3, 8, 6, 20, 15, 10]
+console.log(tree.inOrder()) //  [3, 6, 8, 10, 15, 20]
+
+//              10
+//         6          15
+//       3   8          20
